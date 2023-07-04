b88d1e7013e569ef578e33def60708b18f59521d
Tae-Hyun <kt9811@github.com>
2023-07-05 01:20:31 +0900

    genesis

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+const exteriorPopup = document.querySelector('.exterior_pop');
+const interiorPopup = document.querySelector('.interior_pop');
+const designPopupClose = document.querySelectorAll('.popup_close');
+const exterior = document.querySelector('.exterior');
+const interior = document.querySelector('.interior');
+//외장디자인 팝업
+exterior.addEventListener('click', () => {
+  exteriorPopup.style.opacity = '1';
+  exteriorPopup.style.scale = '1';
+
+  designPopupClose[0].addEventListener('click', () => {
+    exteriorPopup.style.opacity = '0';
+    exteriorPopup.style.scale = '0';
+  });
+
+})
+
+interior.addEventListener('click', () => {
+  interiorPopup.style.opacity = '1';
+  interiorPopup.style.scale = '1';
+
+  designPopupClose[1].addEventListener('click', () => {
+    interiorPopup.style.opacity = '0';
+    interiorPopup.style.scale = '0';
+  });
+  
+})
+
 
 
 const swiper = new Swiper('.genesis_color', {
